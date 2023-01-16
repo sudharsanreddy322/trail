0e8b74194874bf6b67fd70a6291136ed3bfcb067
Venati <223089646@ge.com>
2023-01-16 17:53:25 +0530

    testing...

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        //whole numbers
+        int a = 3;
+        int b = 5;
+
+        //decimal
+        double d = 33.2;
+        float f = 1.2f;
+
+        //boolean
+        boolean b1 = true;
+
+        //character
+        char c = 'A';
+        System.out.println(a+b);
+
+        printHi();
+    }
+
+
+    public static void printHi(){
+        System.out.println("Hi");
+    }
+}
